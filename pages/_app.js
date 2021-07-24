675bbf0e4e5925b49cb8a7020726e8464480182e
Tomas Arras <tomasarras@gmail.com>
2021-07-23 23:11:37 -0300

    change metatag

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
       </Head>
       <UserProvider>
         <Header />
