4fc144f1694c4f404274de54f2f2b4773f944a81
Tomas Arras <tomasarras@gmail.com>
2021-08-03 20:13:59 -0300

    change environment values

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,6 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
       </Head>
       <UserProvider>
         <Header />
